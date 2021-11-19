86013d11f5ffc9214fd3eba79c1da76db5ac7cf1
Yoully Kang <youl.ly.kang@gmail.com>
2021-11-18 21:43:26 -0600

    upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+$("#navProject").on("click", function() {
+    $("#dropDownProject").slideToggle();
+  });
+
+$("#navResume").on("click", function() {
+    $("#dropDownResume").slideToggle();
+  });
+
+});
+
+
+$("li").css("color","#A4905C");
